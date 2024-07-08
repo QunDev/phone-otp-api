0db0bb5157791e81cd9c9c6aac74dc17c4ebace2
QunDev <qun942004Z@gmail.com>
2024-07-08 08:57:21 +0700

    update connect db

--- config/db.js
+++ config/db.js
@@ -3,30 +3,35 @@ const mysql = require('mysql');
 let connection;
 
 function handleDisconnect() {
-  connection = mysql.createConnection({
-    host: '4.213.162.142',
-    user: 'quanph35528',
-    password: 'Qundevauto2k4!',
-    database: 'phone_otp_db'
-  });
+  try {
+    connection = mysql.createConnection({
+      host: '4.213.162.142',
+      user: 'quanph35528',
+      password: 'Qundevauto2k4!',
+      database: 'phone_otp_db'
+    });
 
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting to MySQL:', err);
-      setTimeout(handleDisconnect, 2000); // Attempt to reconnect after 2 seconds
-    } else {
-      console.log('Connected to MySQL');
-    }
-  });
+    connection.connect((err) => {
+      if (err) {
+        console.error('Error connecting to MySQL:', err);
+        setTimeout(handleDisconnect, 2000); // Attempt to reconnect after 2 seconds
+      } else {
+        console.log('Connected to MySQL');
+      }
+    });
 
-  connection.on('error', (err) => {
-    console.error('Database error:', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
+    connection.on('error', (err) => {
+      console.error('Database error:', err);
+      if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
+        handleDisconnect();
+      } else {
+        throw err;
+      }
+    });
+  } catch (error) {
+    console.error('An unexpected error occurred:', error);
+    setTimeout(handleDisconnect, 2000); // Attempt to reconnect after 2 seconds
+  }
 }
 
 handleDisconnect();
