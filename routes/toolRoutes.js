cdf6b52b9c8f664220967b2bb2b57eb5b10ab112
QunDev <qun942004Z@gmail.com>
2024-07-08 23:00:18 +0700

    ...

--- routes/toolRoutes.js
+++ routes/toolRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getVersion, downloadApk } = require('../controllers/yourControllerFile');
+const { getVersion, downloadApk } = require('../controllers/toolController');
 
 // Existing routes
 router.get('/version', getVersion);
